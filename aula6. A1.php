9460b6022f731f7e0d141f90f825193fa7107e7c
LeonardoFlugel <leoflugel2012@hotmail.com>
2025-09-02 15:10:08 -0300

    Update and rename index.php to aula6. A1.php

---
+++ aula6. A1.php
@@ -0,0 +1,13 @@
+<?php
+$cor = "vermelho"; // valor da variável
+
+if ($cor == "vermelho") {
+    echo "Pare!";
+} elseif ($cor == "amarelo") {
+    echo "Atenção!";
+} elseif ($cor == "verde") {
+    echo "Siga!";
+} else {
+    echo "Cor inválida";
+}
+?>
